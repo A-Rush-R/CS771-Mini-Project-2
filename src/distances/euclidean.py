2a59e07a4b9145b7e49e4dfbce8a0d0e72c27b8d
aujasvit <80042330+aujasvit@users.noreply.github.com>
2024-11-08 13:52:48 +0530

    Made code modular

---
+++ src/distances/euclidean.py
@@ -0,0 +1,16 @@
+import torch
+import sklearn
+from distance_base import Distance
+
+class EuclideanDistance(Distance):
+
+    def __init__(self, device) -> None:
+        super().__init__()
+        self.device = device
+
+    def update(self) -> None:
+        pass
+    
+    def distance(x: torch.tensor, y: torch.tensor) -> torch.tensor:
+        x_np, y_np = x.numpy(), y.numpy()
+        return torch.tensor(sklearn.metrics.pairwise.euclidean_distances(x_np, y_np)).to(device)
